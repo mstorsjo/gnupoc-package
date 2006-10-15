f278630782553ffbcad59868bff8c5e0c6d6181d
Martin Storsjo <martin@martin.st>
2006-10-15 00:05:34 +0000

    Fixed the wrapper a little more

--- wrapper.sh
+++ wrapper.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-EKA1TOOLS = ~/symbian-gcc/bin
-EKA2TOOLS = ~/csl-gcc/bin
+EKA1TOOLS=~/symbian-gcc/bin
+EKA2TOOLS=~/csl-gcc/bin
 
 if [ -f $EPOCROOT/epoc32/tools/elf2e32.exe ]; then
   export PATH=$EKA2TOOLS:$EPOCROOT/epoc32/tools:$PATH
