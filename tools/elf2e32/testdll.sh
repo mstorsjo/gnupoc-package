26b7f24cc22bec38c610208c3a48725be4934dec
Martin Storsjo <martin@martin.st>
2007-08-12 12:57:20 +0000

    Add uncompressed to the options

--- tools/elf2e32/testdll.sh
+++ tools/elf2e32/testdll.sh
@@ -2,7 +2,7 @@
 
 INPUT=$1
 
-ARGS="--sid=0x00000000 --uid1=0x10000079 --uid2=0x1000008d --uid3=0x00000000 --vid=0x00000000 --capability=none --fpu=softvfp --targettype=DLL --elfinput=$INPUT --linkas=libname{000a0000}[00000000].dll --libpath=$EPOCROOT/epoc32/release/armv5/lib/"
+ARGS="--uncompressed --sid=0x00000000 --uid1=0x10000079 --uid2=0x1000008d --uid3=0x00000000 --vid=0x00000000 --capability=none --fpu=softvfp --targettype=DLL --elfinput=$INPUT --linkas=libname{000a0000}[00000000].dll --libpath=$EPOCROOT/epoc32/release/armv5/lib/"
 
 wine $EPOCROOT/epoc32/tools/elf2e32.exe --output=e32-ref.dll --defoutput=e32-ref.def --dso=e32-ref.dso --uncompressed $ARGS 
 wine $EPOCROOT/epoc32/tools/elf2e32.exe --e32input=e32-ref.dll > e32info.txt
