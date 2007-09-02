7b74a90c998690696116d0fb217c89cf546c8411
Martin Storsjo <martin@martin.st>
2007-09-02 20:32:35 +0000

    Clarified the license

--- tools/elf2e32/elf2e32.cpp
+++ tools/elf2e32/elf2e32.cpp
@@ -16,6 +16,16 @@
     along with this program; if not, write to the Free Software
     Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 
+    In addition, as a special exception, the copyright holder of this
+    program gives permission to link the code of its release of elf2e32
+    with modified Symbian code under the Symbian Example Source license;
+    and distribute the linked executables. You must obey the GNU General
+    Public License in all respects for all of the code used other the
+    Symbian copyrighted code. If you modify this file, you may extend this
+    exception to your version of the file, but you are not obliged to do
+    so. If you do not wish to do so, delete this exception statement from
+    your version.
+
     Martin Storsjö
     martin@martin.st
 */
