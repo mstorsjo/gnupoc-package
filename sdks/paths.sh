816869dc899427f60c58ad157d0ad1391ecace98
Martin Storsjo <martin@martin.st>
2008-11-30 19:52:44 +0000

    Added a dlloverride for wine for hal.dll

--- sdks/paths.sh
+++ sdks/paths.sh
@@ -9,3 +9,14 @@ else
   export PATH=${EKA1TOOLS}:${EPOCROOT}epoc32/tools:${PATH}
 fi
 
+# winc tools (such as cshlpwtr) use hal.dll from epoc which
+# shouldn't be overridden by the ordinary windows hal.dll
+# provided by wine
+OVERRIDES="hal=n"
+
+if [ -z "$WINEDLLOVERRIDES" ]; then
+	export WINEDLLOVERRIDES="$OVERRIDES"
+else
+	export WINEDLLOVERRIDES="$OVERRIDES;$WINEDLLOVERRIDES"
+fi
+
