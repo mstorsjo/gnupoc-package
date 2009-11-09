6ef3313489cd2fb48ae7b615b16dbd11a656f37c
Martin Storsjo <martin@martin.st>
2009-11-09 13:27:52 +0000

    Compare more aspects of the produced dso

--- tools/elf2e32/testdll.sh
+++ tools/elf2e32/testdll.sh
@@ -6,8 +6,26 @@ ARGS="--uncompressed --sid=0x00000000 --uid1=0x10000079 --uid2=0x1000008d --uid3
 
 wine $EPOCROOT/epoc32/tools/elf2e32.exe --output=e32-ref.dll --defoutput=e32-ref.def --dso=e32-ref.dso --uncompressed $ARGS 
 wine $EPOCROOT/epoc32/tools/elf2e32.exe --e32input=e32-ref.dll > e32info.txt
-./elf2e32 --output=e32-test.dll --defoutput=e32-test.def --dso=e32-test.dso $ARGS
+mv e32-ref.dso e32-ref.dso.orig
+# The dso name is embedded in the file; use the same name to ease diffing
+./elf2e32 --output=e32-test.dll --defoutput=e32-test.def --dso=e32-ref.dso $ARGS
+mv e32-ref.dso e32-test.dso
+mv e32-ref.dso.orig e32-ref.dso
 ./bindiff e32-ref.dll e32-test.dll -i 0x14,0x18 -i 0x24,0x28
 diff -u e32-ref.def e32-test.def
 rm e32-test.dll e32-ref.dll
+
+compare() {
+	CMD=$1
+	ARGS=$2
+	$CMD $ARGS e32-ref.dso > ref-out.txt
+	$CMD $ARGS e32-test.dso | sed s/e32-test.dso/e32-ref.dso/ > test-out.txt
+	diff -u ref-out.txt test-out.txt
+}
+
+# objdump -s produces differences in .dynamic and .hash (and in .version sometimes)
+compare arm-none-symbianelf-objdump "-s -j ER_RO -j .version_d -j .dynsym -j .strtab"
+compare arm-none-symbianelf-readelf -sW 2>/dev/null
+compare arm-none-symbianelf-objdump -r
+compare arm-none-symbianelf-objdump -t
 
