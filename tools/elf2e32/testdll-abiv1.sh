75c080e270f28530fb16bb428a677e7159ec97ae
Martin Storsjo <martin@martin.st>
2009-11-12 12:33:06 +0000

    Reorder bindiff arguments

--- tools/elf2e32/testdll-abiv1.sh
+++ tools/elf2e32/testdll-abiv1.sh
@@ -6,7 +6,7 @@ ARGS="-nocompress -sid 0x00000000 -uid1 0x1000007a -uid2 0x00000000 -uid3 0x0000
 
 wine $EPOCROOT/epoc32/tools/elftran.exe $ARGS e32-ref.dll
 ./elftran $ARGS e32-test.dll
-./bindiff e32-ref.dll e32-test.dll -i 0x14,0x18 -i 0x24,0x28
+./bindiff -i 0x14,0x18 -i 0x24,0x28 e32-ref.dll e32-test.dll
 wine $EPOCROOT/epoc32/tools/elftran.exe e32-ref.dll > e32-ref-dump.txt
 wine $EPOCROOT/epoc32/tools/elftran.exe e32-test.dll > e32-test-dump.txt
 #rm e32-test.dll e32-ref.dll
