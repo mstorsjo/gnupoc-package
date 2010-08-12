bedfae9332cfd03dbe2b35b8d360bafb5f624a94
Martin Storsjo <martin@martin.st>
2010-08-12 06:38:42 +0000

    Extract exe info in testexe.sh

--- tools/elf2e32/testexe.sh
+++ tools/elf2e32/testexe.sh
@@ -7,5 +7,8 @@ ARGS="--uncompressed --sid=0x00000000 --uid1=0x1000007a --uid2=0x00000000 --uid3
 wine $EPOCROOT/epoc32/tools/elf2e32.exe $ARGS --output=e32-ref.exe 
 ./elf2e32 $ARGS --output=e32-test.exe
 ./bindiff -i 0x14,0x18 -i 0x24,0x28 e32-ref.exe e32-test.exe
+wine $EPOCROOT/epoc32/tools/elf2e32.exe --e32input=e32-ref.exe > e32-ref.txt
+wine $EPOCROOT/epoc32/tools/elf2e32.exe --e32input=e32-test.exe > e32-test.txt
+#diff -u e32-ref.txt e32-test.txt
 #rm e32-test.exe e32-ref.exe
 
