9ca9bf241195e260f4844b14cf36742a56e45b75
Martin Storsjo <martin@martin.st>
2011-03-12 00:18:29 +0200

    Don't assume EPOCROOT ends with a slash

--- sdks/gnupoc-common.sh
+++ sdks/gnupoc-common.sh
@@ -3,10 +3,10 @@
 EKA1TOOLS=~/symbian-gcc/bin
 EKA2TOOLS=~/csl-gcc/bin
 
-if [ -f ${EPOCROOT}epoc32/tools/elf2e32.exe ]; then
-	export PATH=${EKA2TOOLS}:${EPOCROOT}epoc32/tools:${PATH}
+if [ -f ${EPOCROOT}/epoc32/tools/elf2e32.exe ]; then
+	export PATH=${EKA2TOOLS}:${EPOCROOT}/epoc32/tools:${PATH}
 else
-	export PATH=${EKA1TOOLS}:${EPOCROOT}epoc32/tools:${PATH}
+	export PATH=${EKA1TOOLS}:${EPOCROOT}/epoc32/tools:${PATH}
 fi
 
 # winc tools (such as cshlpwtr) use hal.dll from epoc which
